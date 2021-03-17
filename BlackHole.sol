9a064cdb04d5fc4d3606811e0ca667a072312e2b
leoyang <ahnujjyang@gmail.com>
2021-03-17 15:06:54 +0800

    init

---
+++ BlackHole.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.12;
+
+// This is a black hole contract address
+// no one can transfer STAR from the contract
+contract BlackHole {
+
+}
+
+/*
+    Star is an automatic market-making decentralized exchange
+
+based on the concept of fund pools.It is similar in function
+
+to some DEXs on the market,but on this basis,it proposes and
+
+implements a dual-chain DEX model based on the Huobi Eco Chain
+
+and Ethereum .It combines the advantages of the low transaction
+
+fees of the Huobi Eco Chain and the prosperity of the Ethereum ecosystem,
+
+and supports the dual mining mechanism of liquidity and transactions.
+
+*/
